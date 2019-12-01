13db9882fe134da7150e6c6aa68b7ee0a36d8add
Firdausfarul <firdausfarul@gmail.com>
2019-12-01 15:33:53 +0700

    Create If Then Else.cpp

---
+++ PemrogamanDasar/05. Percabangan/If Then Else.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a;
+
+int main(){
+  cin >> a;
+	if (a>0){
+		cout << "positif" << endl;
+	}
+  else if (a==0){
+ 	cout << "nol" << endl;	
+  }
+  else if (a<0){
+ 	cout << "negatif" << endl;
+  }
+ }
